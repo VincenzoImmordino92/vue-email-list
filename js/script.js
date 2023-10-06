ab8b627db94ee6ec626d91ebf22bf7c3fb6e254b
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2023-10-06 18:10:54 +0200

    [add] js printEmail html v-for emails

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-  data(){
-    return{
-        apiUrl:'https://flynn.boolean.careers/exercises/api/random/mail',
-        email:''
+  data() {
+    return {
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      emails: []
     }
   },
-  methods:{
-    printEmail(){
-      axios.get(this.apiUrl)
-      .then((response) =>{
-      this.email = response.data.response
-      console.log(email);
-      })
-      .catch((error)=>{
-        console.log(error);
-      })
-    }
-  },
-  mounted(){
+  methods: {
+    printEmail() {
+      for (i = 0; i < 10; i++) {
+        axios.get(this.apiUrl)
+          .then((response) => {
+            this.emails.push(response.data.response)
+            console.log(response.data);
+          })
 
+          .catch((error) => {
+            console.log(error);
+          })
+      }
+    }
+  },
+  mounted() {
+    this.printEmail()
+    this.printMyArray()
   }
 }).mount('#app')
